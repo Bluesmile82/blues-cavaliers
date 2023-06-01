1761551f8eebf64d52df03820ae575bd3385b6e9
Alvaro Leal <pursuance@gmail.com>
2023-06-01 17:20:07 +0200

    Upgrade next

---
+++ components/concerts/concerts-component.tsx
@@ -0,0 +1,40 @@
+interface Concert {
+  name: string;
+  date: string;
+  time: string;
+  price: string;
+  direction: string;
+}
+
+async function Concerts({ promise }: { promise: Promise<Concert[]> }) {
+  // Wait for the concerts promise to resolve
+  const concerts = await promise;
+
+  return (
+    <div className="ml-1 mb-6">
+      Próximos conciertos
+      <ul>
+        {concerts &&
+          concerts.map(
+            (concert) =>
+              concert.name && (
+                <li key={concert.name} className="text-sm text-white">
+                  {concert.date} -{concert.time} -{concert.name} - Precio:{' '}
+                  {concert.price} -{' '}
+                  <a
+                    className="text-blue-500 underline"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={concert.direction}
+                  >
+                    Dirección
+                  </a>
+                </li>
+              ),
+          )}
+      </ul>
+    </div>
+  );
+}
+
+export default Concerts;

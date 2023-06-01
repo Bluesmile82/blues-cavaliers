69b8e8eaf1a6a2ac7976149c4489c3b1f61b4425
Alvaro Leal <pursuance@gmail.com>
2023-06-01 15:51:14 +0200

    Start

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import NotionService from '../services/notion';
+
+export default async function Page() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-xl font-medium text-gray-300">Blues Cavaliers</h1>
+    </div>
+  );
+}
